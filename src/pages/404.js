673ec0e7ce0c01c093df622ea36408913fcd0193
Brian Cruikshank <brian@brianswebstudio.com>
2019-02-26 16:26:05 -0500

    clean up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+  <Layout pageTitle="Not Found">
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <p>You just hit a page that doesn&#39;t exist.. the sandness.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
